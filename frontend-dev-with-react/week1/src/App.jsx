b5c376652e30914beb0cbf3dfb1ab96a2378a199
Umer MiB <mohammadumer704@gmail.com>
2020-10-22 21:45:53 +0500

    Presentational and Container Components

--- frontend-dev-with-react/week1/src/App.jsx
+++ frontend-dev-with-react/week1/src/App.jsx
@@ -1,18 +1,14 @@
-import React, {Component} from 'react';
-import './App.css';
-import Menu from './Components/MenuComponent.jsx';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from "react";
+import "./App.css";
+import Main from "./components/MainComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu/>
+        <div className="App">
+          <Main />
+        </div>
       </div>
     );
   }
